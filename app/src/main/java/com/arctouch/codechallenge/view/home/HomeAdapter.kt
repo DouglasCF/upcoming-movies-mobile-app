1b69109b07b759524402afa946d7642d9b0e39a7
Douglas Fornaro <douglasfornaro@hotmail.com>
2018-08-04 13:40:35 -0300

    Adding progress while loading more movies

--- app/src/main/java/com/arctouch/codechallenge/view/home/HomeAdapter.kt
+++ app/src/main/java/com/arctouch/codechallenge/view/home/HomeAdapter.kt
@@ -2,17 +2,24 @@ package com.arctouch.codechallenge.view.home
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.databinding.MovieItemBinding
 import com.arctouch.codechallenge.model.Movie
 
-class HomeAdapter(private val listener: OnHomeListener) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter(private val listener: OnHomeListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     interface OnHomeListener {
         fun onClick(movie: Movie)
     }
 
-    private val movies = mutableListOf<Movie>()
+    companion object {
+        private const val ITEM = 0
+        private const val PROGRESS = 1
+    }
+
+    private val movies = mutableSetOf<Movie?>()
 
     class ViewHolder(private val binding: MovieItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
@@ -24,14 +31,29 @@ class HomeAdapter(private val listener: OnHomeListener) : RecyclerView.Adapter<H
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    class ProgressViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ITEM -> {
+                val binding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ViewHolder(binding)
+            }
+            else -> {
+                ProgressViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_progress, parent, false))
+            }
+        }
     }
+
+    override fun getItemViewType(position: Int) = if (movies.elementAt(position) != null) ITEM else PROGRESS
 
     override fun getItemCount() = movies.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(movies[position], listener)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            ITEM -> (holder as ViewHolder).bind(movies.elementAt(position)!!, listener)
+        }
+    }
 
     fun setData(data: List<Movie>) {
         movies.clear()
@@ -39,8 +61,13 @@ class HomeAdapter(private val listener: OnHomeListener) : RecyclerView.Adapter<H
         notifyDataSetChanged()
     }
 
-    fun addData(data: List<Movie>) {
-        movies.addAll(data)
+    fun addData(data: List<Movie>?) {
+        if (data == null) {
+            movies.add(null)
+        } else {
+            movies.remove(null)
+            movies.addAll(data)
+        }
         notifyDataSetChanged()
     }
 }
