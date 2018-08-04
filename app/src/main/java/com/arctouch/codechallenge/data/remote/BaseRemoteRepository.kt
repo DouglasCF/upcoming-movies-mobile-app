1e1dc67e7668096692075a4def98a6fe4d4c0ab5
Douglas Fornaro <douglasfornaro@hotmail.com>
2018-08-04 18:00:18 -0300

    Removing RxJava

--- app/src/main/java/com/arctouch/codechallenge/data/remote/BaseRemoteRepository.kt
+++ app/src/main/java/com/arctouch/codechallenge/data/remote/BaseRemoteRepository.kt
@@ -3,7 +3,6 @@ package com.arctouch.codechallenge.data.remote
 import com.arctouch.codechallenge.api.TmdbApi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 abstract class BaseRemoteRepository {
@@ -11,7 +10,6 @@ abstract class BaseRemoteRepository {
             .baseUrl(TmdbApi.URL)
             .client(OkHttpClient.Builder().build())
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
             .create(TmdbApi::class.java)
 }
