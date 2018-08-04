75dfd8c4eb44d295288aa49e092e55b2be8eb650
Douglas Fornaro <douglasfornaro@hotmail.com>
2018-08-03 23:38:56 -0300

    Showing progress bar while loading movie detail

--- app/src/main/java/com/arctouch/codechallenge/view/BindingAdapters.kt
+++ app/src/main/java/com/arctouch/codechallenge/view/BindingAdapters.kt
@@ -2,6 +2,7 @@ package com.arctouch.codechallenge.view
 
 import android.databinding.BindingAdapter
 import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -14,5 +15,11 @@ object BindingAdapters {
         val options = RequestOptions().placeholder(placeholder)
         if (centerCrop) options.centerCrop()
         Glide.with(context).load(url).apply(options).into(this)
+    }
+
+    @JvmStatic
+    @BindingAdapter("showVisible")
+    fun View.showVisible(show: Boolean) {
+        visibility = if (show) View.VISIBLE else View.GONE
     }
 }
